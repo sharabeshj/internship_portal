722d2139bbc716aa1f2a14e36245bb0fe7d38413
sharabesh <sharabeshjayaraman@gmail.com>
2019-09-21 06:31:46 +0530

    initial commit

---
+++ db/migrations/20190920184724_users.php
@@ -0,0 +1,51 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $users_table = $this->table('users');
+        $student_table = $this->table('student');
+        $employer_table = $this->table('employer');
+
+        $users_table->addColumn('username','string',['limit' => 50])
+                ->addColumn('role','string', ['limit' => 30])
+                ->create();
+        $student_table->addForeignKey('id','users','id',['delete' => 'CASCADE', 'update' => 'CASCADE'])
+                    ->addColumn('school','string',['limit' => 100])
+                    ->addColumn('resume', 'blob', ['limit' => MysqlAdapter::BLOB_MEDIUM])
+                    ->create();
+        $employer_table->addForeignKey('id','users','id',['delete' => 'CASCADE', 'update' => 'CASCADE'])
+                    ->addColumn('name', 'string',['limit' => 50])
+                    ->addColumn('about','text',['limit' => MysqlAdapter::TEXT_LONG])
+                    ->create();
+    }
+}
---
+++ route.php
@@ -0,0 +1,86 @@
+<?php
+class Route{
+    
+    // A base class for router. We will keep on adding routes in index
+    // and their respective function handlers 
+
+    private static $routes = Array();
+    private static $pathNotFound = null;
+    private static $methodNotAllowed = null;
+
+    public static function add($exp, $func, $method = 'GET'){
+        array_push(self::$routes, Array(
+            'expression' => $exp,
+            'function' => $func,
+            'method' => $method
+        ));
+    }
+
+    public static function pathNotFound($func){
+        self::$pathNotFound = $func;
+    }
+
+    public static function methodNotAllowed($func){
+        self::$methodNotAllowed = $func;
+    }
+
+    public static function run($basepath = '/'){
+        //parse the url
+        $parsed_url = parse_url($_SERVER['REQUEST_URI']);
+        
+        if(isset($parsed_url['path'])){
+            $path = $parsed_url['path'];
+        }else{
+            $path = '/';
+        }
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $path_match_found = false;
+
+        $route_match_found = false;
+
+        foreach(self::$routes as $route){
+            //check for matching function handler
+            if($basepath != '' && $basepath != '/'){
+                $route['expression'] = '('.$basepath.')'.$route['expression'];
+            }
+
+            $route['expression'] = '^'.$route['expression'].'$';
+            
+            if(preg_match('#'.$route['expression'].'#', $path, $matches)){
+                $path_match_found = true;
+
+                if(strtolower($method) == strtolower($route['method'])){
+                    array_shift($matches);
+
+                    if($basepath != '' && $basepath != '/'){
+                        array_shift($matches);
+                    }
+
+                    if(!call_user_func_array($route['function'], $matches)){
+                        break;
+                    }else{
+                        $route_match_found = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(!$route_match_found){
+            if(!$path_match_found){
+                header("HTTP/1.0 405 Method Not Allowed");
+                if(self::$methodNotAllowed){
+                    call_user_func_array(self::$methodNotAllowed, Array($path, $method));
+                }
+            }else{
+                header("HTTP/1.0 404 Not Found");
+                if(self::$pathNotFound){
+                    call_user_func_array(self::$pathNotFound, Array($path));
+                }
+            }
+        }
+    }
+}
+?>
